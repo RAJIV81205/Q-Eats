b33d66b9687dd065a044e0b76fe1ac31f41c4290
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2024-10-04 23:13:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+    document.getElementById("loader").style.display = "none";
+  });
+
+  const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
